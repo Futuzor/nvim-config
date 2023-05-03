6dfbaa3940f1cc924f2060bc41b5d240ca78c04b
Futuzor <futuzormekkla@gmail.com>
2023-05-03 15:30:53 +0200

    Codi config fix

--- .config/nvim/lua/codiconf.lua
+++ .config/nvim/lua/codiconf.lua
@@ -1,5 +1,10 @@
 --Este es el archivo de configuración del plugin Codi. Abajo se configuran los
 --diferentes intérpretes de los lenguajes de programación.
-local python = "let g:codi#interpreters = { \\ 'python': { \\ 'bin': 'python', \\ 'prompt': '^\\(>>>\\|\\.\\.\\.\\) ', \\ }, \\ }"
-
-vim.cmd("let g:codi#interpreters = {python}")
+vim.cmd [[
+    let g:codi#interpreters = {
+                   \ 'python': {
+                       \ 'bin': $PYTHONBIN,
+                       \ 'prompt': '^\(>>>\|\.\.\.\) ',
+                       \ },
+                   \ }
+]]
