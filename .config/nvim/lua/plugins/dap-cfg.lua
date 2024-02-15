59b3efcb68eea71567c4eac92d0d96a4e6bcdf2d
Futuzor <113979181+Futuzor@users.noreply.github.com>
2024-02-15 10:16:32 +0100

    Actualizado el dap-cfg para configuración del DAP en JS/TS

--- .config/nvim/lua/plugins/dap-cfg.lua
+++ .config/nvim/lua/plugins/dap-cfg.lua
@@ -1,99 +1,75 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "mxsdev/nvim-dap-vscode-js",
-        "mfussenegger/nvim-dap-python",
-        "theHamsta/nvim-dap-virtual-text",
-    },
-    event = "BufReadPost",
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        local vt = require("nvim-dap-virtual-text")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"mxsdev/nvim-dap-vscode-js",
+		"mfussenegger/nvim-dap-python",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	event = "BufReadPost",
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-        dapui.setup()
-        vt.setup()
+		dapui.setup()
 
-        -- DAP-UI
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close()
-        end
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
 
-        require("dap-python").setup("/usr/bin/python3")
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
 
-        --[[require("dap-vscode-js").setup({
-            --node_path = "~/.bun/bin/bun", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-            --debugger_path = vim.fn.stdpath('data') .. "/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
-            debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-              adapters = { "pwa-node" },
-            --adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-        })]]
-        --
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
 
-        dap.adapters["pwa-node"] = {
-            type = "server",
-            host = "127.0.0.1",
-            port = 8123,
-        }
+		-- Configuración Python
+		require("dap-python").setup("/home/futuzor/Documentos/Python/debugpy/bin/python")
 
-        dap.adapters["pwa-chrome"] = {
-            type = "server",
-            host = "127.0.0.1",
-            port = 8124,
-        }
+		-- Configuración JavaScript/TypeScript
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "localhost",
+			port = 8123,
+			executable = {
+				--command = "~/.dap/vscode-js-debug/out/src/vsDebugServer.js", -- Path to VSCode Debugger
+				command = "js-debug-adapter",
+			},
+		}
 
-        dap.adapters["pwa-msedge"] = {
-            type = "server",
-            host = "127.0.0.1",
-            port = 8125,
-        }
+		--[[require("dap-vscode-js").setup({
+			node_path = "node",                                                                          -- Path of node executable. Defaults to $NODE_PATH, and then $VIMRUNTIME/lua/?.lua
+			debugger_path = "/home/futuzor/.dap/vscode-js-debug",
+			debugger_cmd = { "node", "/home/futuzor/.dap/vscode-js-debug/out/src/vsDebugServer.js" },    -- Path to vscode-js-debug installation.
+			adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+		})--]]
 
-        for _, language in ipairs({ "typescript", "javascript", "typescriptreact" }) do
-            dap.configurations[language] = {
-                {
-                    type = "pwa-node",
-                    request = "launch",
-                    name = "Launch file",
-                    program = "${file}",
-                    sourceMaps = true,
-                    cwd = vim.fn.getcwd(),
-                    runtimeExecutable = "ts-node",
-                },
-                {
-                    type = "pwa-chrome",
-                    request = "launch",
-                    name = "Launch file",
-                    program = "${file}",
-                    sourceMaps = true,
-                    cwd = vim.fn.getcwd(),
-                    runtimeExecutable = "ts-node",
-                },
-                {
-                    type = "pwa-msedge",
-                    request = "launch",
-                    name = "Launch file",
-                    program = "${file}",
-                    sourceMaps = true,
-                    cwd = vim.fn.getcwd(),
-                    runtimeExecutable = "ts-node",
-                },
-            }
-        end
+		for _, language in ipairs({ "typescript", "javascript" }) do
+			dap.configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file in Node",
+					program = "${file}",
+					cwd = vim.fn.getcwd(),
+					runtimeExecutable = "ts-node",
+				},
+			}
+		end
+	end,
 
-        -- KEYMAPS
-        vim.keymap.set(
-            "n",
-            "<Leader>db",
-            ":DapToggleBreakpoint<cr>",
-            { silent = true, desc = "Añadir punto de ruptura" }
-        )
-        vim.keymap.set("n", "<Leader>dc", ":DapContinue<cr>", { silent = true, desc = "Comenzar depuración" })
-    end,
+	vim.keymap.set(
+		"n",
+		"<Leader>db",
+		":lua require'dap'.toggle_breakpoint()<CR>",
+		{ silent = true, desc = "Toggle Breakpoint" }
+	),
+	vim.keymap.set(
+		"n",
+		"<Leader>dc",
+		":lua require'dap'.continue()<CR>",
+		{ silent = true, desc = "Debug Start/Continue" }
+	),
 }
